042219054f69faa5373b297e2304ab8f99b3b1aa
acoulton <andrew@ingenerator.com>
2022-11-11 11:22:15 +0000

    Connect a logger to php-vips to show debug information
    Added separately to prove the segfault occurs with or
    without the logger connected.

--- html/test.php
+++ html/test.php
@@ -5,10 +5,24 @@
 //
 // See index.php for the actual VIPS operations
 
+use Jcupitt\Vips\Config;
+use Psr\Log\AbstractLogger;
+
 try {
     set_error_handler(
         fn($errno, $errstr, $errfile, $errline) => throw new ErrorException($errstr, $errno, $errno, $errfile, $errline)
     );
+
+    require_once(__DIR__.'/../vendor/autoload.php');
+
+    Config::setLogger(new class extends AbstractLogger {
+        public function log($level, \Stringable|string $message, array $context = []): void
+        {
+            $context = [] === $context ? '' : ' '.json_encode($context);
+            $line = "[vips - $level] $message".$context;
+            printLog(substr($line, 0, 100));
+        }
+    });
 
     // Actually run the VIPS operations from the reproduction
     require_once __DIR__.'/index.php';
